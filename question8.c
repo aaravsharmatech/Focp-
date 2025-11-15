78eda36dd25566ff1b884c47b9e889492b42ac7e
aaravsharmatech <123654135+aaravsharmatech@users.noreply.github.com>
2025-11-15 23:38:40 +0530

    Create question8.c

---
+++ question8.c
@@ -0,0 +1,24 @@
+q8
+#include <stdio.h>
+int main(){
+    int n,first=0,second=1,next,i;
+
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        if(i==0){
+            next = 0;
+        }
+        else if(i==1){
+            next = 1;
+        }
+        else{
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d, ",next);
+    }
+    return 0;
+}
